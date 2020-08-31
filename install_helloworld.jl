bb57437a9a14d79ad0a20862383948ef4c80b58a
Kailai Xu <kailaix@hotmail.com>
2020-08-30 23:55:09 -0700

    update

--- install_helloworld.jl
+++ install_helloworld.jl
@@ -1,5 +1,16 @@
 using ADCME
 
-ADCME.PREFIXDIR
-
-git_repository()
+PWD = pwd()
+change_directory(ADCME.PREFIXDIR)
+git_repository("https://github.com/kailaix/LibHelloWorld", "LibHelloWorld")
+change_directory("LibHelloWorld")
+make_directory("build")
+change_directory("build")
+lib = get_library("hello_world")
+require_file(lib) do 
+    ADCME.cmake()
+    ADCME.make()
+end
+_, libname = splitdir(lib)
+mv(lib, joinpath(ADCME.LIBDIR, libname))
+change_directory(PWD)
